fde61efeecaff2abb346aff3cbf2040beada7d41
svjson <johansson.sven@gmail.com>
2024-10-04 21:21:39 +0200

    Added test for TakeFunction

--- test/tests/lang_test.cpp
+++ test/tests/lang_test.cpp
@@ -1182,6 +1182,16 @@ TEST(RandNth, single_element)
   }
 }
 
+TEST(TakeFunction, take)
+{
+  Lisple::LispReader runtime;
+
+  EXPECT_EQ(runtime.eval("(take 2 [1 2 3 4 5 6 7 8 9 10])")->to_string(), "[1 2]");
+  EXPECT_EQ(runtime.eval("(take 5 [1 2 3])")->to_string(), "[1 2 3]");
+  EXPECT_EQ(runtime.eval("(take 10 [])")->to_string(), "[]");
+  EXPECT_EQ(runtime.eval("(take 0 [1 2 3])")->to_string(), "[]");
+}
+
 TEST(RepeatFunction, repeat)
 {
   Lisple::LispReader runtime;
