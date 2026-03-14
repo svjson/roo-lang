15fb0d00f758603aebda000afce6fb7ed90bcd5c
Sven Johansson <johansson.sven@gmail.com>
2026-03-14 14:49:54 +0100

    Added MultiRef metatype for seq/string

--- src/lisple/type.h
+++ src/lisple/type.h
@@ -135,6 +135,8 @@ namespace Lisple
 
     inline const MultiRef SEQ(std::vector<const TypeRef*>{&LIST, &ARRAY, &MAP, &HOST_SEQ},
                               "Seq");
+    inline const MultiRef SEQ_OR_STRING(std::vector<const TypeRef*>({&SEQ, &STRING}),
+                                        "SeqLike");
     inline const MultiRef EXEC(std::vector<const TypeRef*>{&FUNCTION, &MACRO, &KEY}, "Exec");
     inline const MultiRef COMPLEX(std::vector<const TypeRef*>{&MAP, &HOST_OBJECT},
                                   "Complex");
