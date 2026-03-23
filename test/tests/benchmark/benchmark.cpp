3b471bc0cc3e87c296590197a5b1b3e94da293d7
Sven Johansson <johansson.sven@gmail.com>
2026-03-23 16:58:45 +0100

    Added benchmark userfunction path fix

--- test/tests/benchmark/benchmark.cpp
+++ test/tests/benchmark/benchmark.cpp
@@ -97,7 +97,7 @@ namespace LispleTest
 
   void SnippetBenchmark::log_result()
   {
-    const std::string CHANGE_ME = "016_operator_rtval_implementations_84c39a1";
+    const std::string CHANGE_ME = "017_user_function_path_fix_44790d8";
     const std::string dir = "benchmarks/" + CHANGE_ME;
     const std::string file_name = dir + "/" + case_name + ".csv";
 
