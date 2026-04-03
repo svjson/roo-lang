255010e5d56bb668ae125c917dba2d12566b4a69
Sven Johansson <johansson.sven@gmail.com>
2026-04-04 01:21:09 +0200

    Added benchmarks for execnode-based do form

--- test/tests/benchmark/benchmark.cpp
+++ test/tests/benchmark/benchmark.cpp
@@ -252,7 +252,7 @@ namespace LispleTest
 
   void SnippetBenchmark::log_result()
   {
-    const std::string CHANGE_ME = "028_zerop_function_04553bd";
+    const std::string CHANGE_ME = "029_execnode_do_form_5e0a9fd";
     const std::string dir = "benchmarks/" + CHANGE_ME;
     const std::string file_name = dir + "/" + case_name + ".csv";
 
