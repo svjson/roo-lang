f7051f13152f6b7331414c9cb5cb02a831216350
Sven Johansson <johansson.sven@gmail.com>
2026-04-04 19:36:41 +0200

    Added benchmarks and metrics for lowering, separately for setup phasse and exec phase

--- test/tests/benchmark/benchmark.cpp
+++ test/tests/benchmark/benchmark.cpp
@@ -329,7 +329,7 @@ namespace LispleTest
 
   void SnippetBenchmark::log_result()
   {
-    const std::string CHANGE_ME = "031_set_bang_special_form_eb385aa";
+    const std::string CHANGE_ME = "032_lower_time_exec_resolution_682bb0d";
     const std::string dir = "benchmarks/" + CHANGE_ME;
     const std::string file_name = dir + "/" + case_name + ".csv";
 
