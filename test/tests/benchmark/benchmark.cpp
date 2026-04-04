2d214d3d4f1830f916042ebc303a4bec9f16ac68
Sven Johansson <johansson.sven@gmail.com>
2026-04-04 17:54:47 +0200

    Added benchmarks for set! execnode implementation

--- test/tests/benchmark/benchmark.cpp
+++ test/tests/benchmark/benchmark.cpp
@@ -274,7 +274,7 @@ namespace LispleTest
 
   void SnippetBenchmark::log_result()
   {
-    const std::string CHANGE_ME = "030_multiplication_type_promotion_37a9b05";
+    const std::string CHANGE_ME = "031_set_bang_special_form_eb385aa";
     const std::string dir = "benchmarks/" + CHANGE_ME;
     const std::string file_name = dir + "/" + case_name + ".csv";
 
