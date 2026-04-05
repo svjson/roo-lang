cdd9863a75f97ebc1c69dbd2ffdf24c4c7f0a6d0
Sven Johansson <johansson.sven@gmail.com>
2026-04-06 01:41:00 +0200

    Added benchmark for vectory copy in nth and last

--- test/tests/benchmark/benchmark.cpp
+++ test/tests/benchmark/benchmark.cpp
@@ -358,7 +358,7 @@ namespace LispleTest
 
   void SnippetBenchmark::log_result()
   {
-    const std::string CHANGE_ME = "049_avoid_vector_copy_in_return_4ea934f";
+    const std::string CHANGE_ME = "050_nth_and_last_vector_copy_fix_875f708";
     const std::string dir = "benchmarks/" + CHANGE_ME;
     const std::string file_name = dir + "/" + case_name + ".csv";
 
