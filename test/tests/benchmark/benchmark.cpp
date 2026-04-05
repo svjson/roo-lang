3ff60586d231c2583fb9e4bf558cffd26b2607cc
Sven Johansson <johansson.sven@gmail.com>
2026-04-05 04:01:42 +0200

    Added benchmarks for pre-lowered let

--- test/tests/benchmark/benchmark.cpp
+++ test/tests/benchmark/benchmark.cpp
@@ -348,7 +348,7 @@ namespace LispleTest
 
   void SnippetBenchmark::log_result()
   {
-    const std::string CHANGE_ME = "036_lambda_node_326e261";
+    const std::string CHANGE_ME = "037_pre_lower_let_a589aac";
     const std::string dir = "benchmarks/" + CHANGE_ME;
     const std::string file_name = dir + "/" + case_name + ".csv";
 
