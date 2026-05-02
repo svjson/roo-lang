a15c28a05d09be9e4ff6597db35da23357dfa82a
Sven Johansson <johansson.sven@gmail.com>
2026-05-02 20:55:11 +0200

    Added tests verifying get behavior with dynamic and qualified keywords

--- test/tests/lang/get_test.cpp
+++ test/tests/lang/get_test.cpp
@@ -15,3 +15,35 @@ TEST(GetFunction, get_from_map)
   // Then
   ASSERT_EQ(result->i64(), 2);
 }
+
+TEST(GetFunction, get_dynamic_value_from_map)
+{
+  // Given
+  Lisple::Runtime runtime;
+  runtime.eval("(def val {:type-1 :a :type-2 :b})");
+  runtime.eval("(def my-map {:a 1 :b 2})");
+
+  // When
+  auto result1 = runtime.eval("(get my-map (:type-1 val))");
+  auto result2 = runtime.eval("(get my-map (:type-2 val))");
+
+  // Then
+  ASSERT_EQ(result1->i32(), 1);
+  ASSERT_EQ(result2->i32(), 2);
+}
+
+TEST(GetFunction, get_dynamic_value_from_map_by_qualified_key)
+{
+  // Given
+  Lisple::Runtime runtime;
+  runtime.eval("(def val {:type-1 :type/a :type-2 :type/b})");
+  runtime.eval("(def my-map {:type/a 1 :type/b 2})");
+
+  // When
+  auto result1 = runtime.eval("(get my-map (:type-1 val))");
+  auto result2 = runtime.eval("(get my-map (:type-2 val))");
+
+  // Then
+  ASSERT_EQ(result1->i32(), 1);
+  ASSERT_EQ(result2->i32(), 2);
+}
