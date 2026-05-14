2fc516e8c21e670663aa251943c2d586e3ca45e1
Sven Johansson <johansson.sven@gmail.com>
2026-05-14 18:41:35 +0200

    Added benchmark for snapshot

--- test/tests/benchmark/benchmark.cpp
+++ test/tests/benchmark/benchmark.cpp
@@ -358,7 +358,7 @@ namespace LispleTest
 
   void SnippetBenchmark::log_result()
   {
-    const std::string CHANGE_ME = "055_all_lang_symbols_on_rt_path_cde3a06";
+    const std::string CHANGE_ME = "056_refactor_snapshot_6f58e4d";
     const std::string dir = "benchmarks/" + CHANGE_ME;
     const std::string file_name = dir + "/" + case_name + ".csv";
 
