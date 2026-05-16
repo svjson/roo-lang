77acf343a7701528f15df3491a0106d4a8f2af21
Sven Johansson <johansson.sven@gmail.com>
2026-05-16 17:32:43 +0200

    Make equality structural between map<->adapter<->adapter

--- lib/liblisple/src/lisple/runtime/value.cpp
+++ lib/liblisple/src/lisple/runtime/value.cpp
@@ -619,7 +619,9 @@ namespace Lisple
 
   bool Value::operator==(const Value& other) const
   {
-    if (this->type != other.type) return false;
+    if (this->type != other.type && this->type != Type::NATIVE_OBJECT &&
+        this->type != Type::MAP)
+      return false;
 
     switch (this->type)
     {
@@ -637,6 +639,15 @@ namespace Lisple
     case Type::SYMBOL:
       return std::get<std::string>(this->value) == std::get<std::string>(other.value);
     case Type::MAP:
+      if (other.type == Type::NATIVE_OBJECT)
+      {
+        return other == *this;
+      }
+      else if (other.type != Type::MAP)
+      {
+        return false;
+      }
+      [[fallthrough]];
     case Type::LIST:
     case Type::VECTOR:
     {
@@ -645,14 +656,50 @@ namespace Lisple
       if (a.size() != b.size()) return false;
       for (size_t i = 0; i < a.size(); i++)
       {
-        if (*a[i] != *b[i]) return false;
+        if (!(*a[i] == *b[i])) return false;
       }
       return true;
     }
     case Type::NATIVE_OBJECT:
     {
-      if (this->nobj()->get_host_type() != other.nobj()->get_host_type()) return false;
-      return this->to_string() == other.to_string();
+      if (other.type == Type::MAP)
+      {
+        auto& map_elements = other.elements();
+        auto& native_object = *this->nobj();
+        const auto& native_keys = native_object.accessor_table().keys;
+        if (map_elements.size() != native_keys.size() * 2) return false;
+
+        for (size_t i = 0; i < native_keys.size(); i++)
+        {
+          const size_t key_index = i * 2;
+          const size_t value_index = key_index + 1;
+          if (!(*map_elements[key_index] == *native_keys[i])) return false;
+          if (!(*map_elements[value_index] == *native_object.get_property(*native_keys[i])))
+            return false;
+        }
+
+        return true;
+      }
+      else if (other.type == Type::NATIVE_OBJECT)
+      {
+        auto& a = *this->nobj();
+        auto& b = *other.nobj();
+        const auto& a_keys = a.accessor_table().keys;
+        const auto& b_keys = b.accessor_table().keys;
+        if (a_keys.size() != b_keys.size()) return false;
+
+        for (size_t i = 0; i < a_keys.size(); i++)
+        {
+          if (!(*a_keys[i] == *b_keys[i])) return false;
+          if (!(*a.get_property(*a_keys[i]) == *b.get_property(*b_keys[i]))) return false;
+        }
+
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
     default:
       throw LispleException("== not implemented for type: " +
--- lib/liblisple/test/tests/host/native_object_vehicle_usage_test.cpp
+++ lib/liblisple/test/tests/host/native_object_vehicle_usage_test.cpp
@@ -1,8 +1,7 @@
 
-#include "runtime_fixture.h"
-
 #include "host/test_adapters/vectorgfx_native_adapters.h"
 #include "host/test_adapters/vehicle_native_adapters.h"
+#include "runtime_fixture.h"
 #include "test_adapters/vehicle_native_adapters.h"
 #include <gtest/gtest.h>
 
@@ -121,4 +120,22 @@ TEST_F(NativeObjectAdapter_usage, rt_dispatch_coerces_args)
   auto& point = result->adapter<LispleTest::Native::PointAdapter>().get_object();
   EXPECT_EQ(point.x, 4);
   EXPECT_EQ(point.y, 6);
+}
+
+TEST_F(NativeObjectAdapter_usage, equality_compares_native_adapter_to_map_shape)
+{
+  std::vector<std::unique_ptr<Lisple::Namespace>> namespaces;
+  namespaces.push_back(std::make_unique<LispleTest::Native::PointNamespace>("pixils.point"));
+  auto& runtime = use_runtime_with(std::move(namespaces), nullptr);
+
+  runtime.eval("(def point (pixils.point/make-point {:x 10 :y 15}))");
+
+  EXPECT_EQ(*runtime.eval("(= point {:x 10 :y 15})"), *Lisple::Constant::BOOL_TRUE);
+  EXPECT_EQ(*runtime.eval("(= {:x 10 :y 15} point)"), *Lisple::Constant::BOOL_TRUE);
+  EXPECT_EQ(*runtime.eval("(= point (pixils.point/make-point {:x 10 :y 15}))"),
+            *Lisple::Constant::BOOL_TRUE);
+
+  EXPECT_EQ(*runtime.eval("(= point {:x 10 :y 16})"), *Lisple::Constant::BOOL_FALSE);
+  EXPECT_EQ(*runtime.eval("(= point {:x 10})"), *Lisple::Constant::BOOL_FALSE);
+  EXPECT_EQ(*runtime.eval("(= point {:x 10 :y 15 :z 0})"), *Lisple::Constant::BOOL_FALSE);
 }
