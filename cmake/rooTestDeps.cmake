e814e8a3029b1d9b8449abdef03f997a37b93826
Sven Johansson <johansson.sven@gmail.com>
2026-07-22 02:11:42 +0200

    build: set DOWNLOA_EXTRACT_TIMESTAMP and silence warnings

--- cmake/rooTestDeps.cmake
+++ cmake/rooTestDeps.cmake
@@ -7,6 +7,7 @@ if(NOT GTest_FOUND)
   FetchContent_Declare(
     googletest
     URL https://github.com/google/googletest/archive/refs/tags/v1.15.2.zip
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
   )
   FetchContent_MakeAvailable(googletest)
 endif()
