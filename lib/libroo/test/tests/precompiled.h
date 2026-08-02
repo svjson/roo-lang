27eba04e29521ca4f2bec3f2105329c91e8f673f
Sven Johansson <johansson.sven@gmail.com>
2026-08-02 15:16:37 +0200

    libroo: Added precompiled headers for test suite

---
+++ lib/libroo/test/tests/precompiled.h
@@ -0,0 +1,17 @@
+#ifndef ROO_TEST_PRECOMPILED_H
+#define ROO_TEST_PRECOMPILED_H
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <roo/adapter.h>
+#include <roo/exception.h>
+#include <roo/runtime/seq.h>
+
+#include "runtime_fixture.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#endif
