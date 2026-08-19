539ac1ce9f23b7cb8ec3def20a0897a14e3fa4a4
Sven Johansson <johansson.sven@gmail.com>
2026-08-19 03:06:42 +0200

    lookup: Refactor source parsing and resolving into focused namespaces

--- pkg/lookup/test/assets/native-package/native/src/sample.cpp
+++ pkg/lookup/test/assets/native-package/native/src/sample.cpp
@@ -4,4 +4,15 @@ namespace Sample
 {
   /** SampleReadFunction - sample.native/read! */
   FUNC_IMPL(SampleReadFunction, SIG((NO_ARGS, EXEC_DISPATCH(&SampleReadFunction::exec_read))))
+
+  /*!
+   * @roo.symbol sample.native/write!
+   * @roo.kind function
+   * @brief Write fixture data to a path.
+   *
+   * | Arg  | Description    |
+   * | ---- | -------------- |
+   * | path | Path to write. |
+   */
+  ns->store("write!", SampleWriteFunction::make());
 } // namespace Sample
