32d7ba0ed81393c612d0023869d369ce1eb2a1c5
Sven Johansson <johansson.sven@gmail.com>
2026-09-10 19:50:47 +0200

    inpoots: Adapt to load-only lifecycle

--- pkg/inpoots/native/src/native_package.cpp
+++ pkg/inpoots/native/src/native_package.cpp
@@ -16,8 +16,6 @@ namespace
     return 0;
   }
 
-  void unload_inpoots_native() {}
-
   const char* inpoots_native_last_error()
   {
     return "";
@@ -33,7 +31,6 @@ extern "C" ROO_NATIVE_EXPORT const RooNativePackageV1* roo_native_package_v1()
     "0.1.0",
     ROO_NATIVE_CXX_ABI,
     load_inpoots_native,
-    unload_inpoots_native,
     inpoots_native_last_error,
   };
   return &package;
